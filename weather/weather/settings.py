f1f2548a555bc1408f0947cec765d3ed9fe4a12b
Kristina-Stukun <73112738+Kristina-Stukun@users.noreply.github.com>
2022-03-07 12:24:04 +0500

    Update settings.py

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%g^w#kua)sxu^z+i61ub7ildklveq73i%_@z-p@q-etrg&1^a'
+SECRET_KEY = 'django-insecure-&%g^w#kua)sxu^z+i61ub7ilffffff3i%_@z-p@q-etrg&1^a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
